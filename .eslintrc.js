460641c13b2010443eefe8d5c39485b08958c320
G1edriusP <giedrius.pakalniskis@gmail.com>
2024-03-25 13:05:29 +0200

    eslint, prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,55 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'simple-import-sort'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^react'],
+          ['@?\\w*-\\w*|(?!react\b)\b\\w+'],
+          ['@assets'],
+          ['@styles'],
+          ['@components'],
+          ['@screens'],
+          ['@data'],
+          ['@utils'],
+          ['@api'],
+          ['@navigation'],
+          ['@translation'],
+          ['^[./]'],
+        ],
+      },
+    ],
+  },
 };
